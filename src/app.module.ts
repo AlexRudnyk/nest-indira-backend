eb058e1179848d06532cf136da1e148f4944145b
AlexRudnyk <alexander.rudnyk@ukr.net>
2024-12-06 15:25:00 +0200

    hidden connection url to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
-// import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ProductsModule,
-    // ConfigModule.forRoot({
-    //   envFilePath: '.env',
-    // }),
-    MongooseModule.forRoot(
-      'mongodb+srv://Alexander:mI8hedGHekO5mkO8@cluster0.nxnowzl.mongodb.net/indira?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(process.env.DB_HOST),
   ],
   controllers: [],
   providers: [],
