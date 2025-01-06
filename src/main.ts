177b7ee5487df222453045f662da851c3b2bbccc
AlexRudnyk <alexander.rudnyk@ukr.net>
2025-01-06 15:59:12 +0200

    fixes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*', credentials: true });
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3001);
 }
