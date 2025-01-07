4280dadac42ca7b28c759eb59cb0be6550347094
AlexRudnyk <alexander.rudnyk@ukr.net>
2025-01-07 14:08:34 +0200

    modified vercel.json

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // app.enableCors({
-  //   origin: ['http://localhost:3000'],
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: true,
-  //   allowedHeaders: 'Content-Type, Accept, Authorization',
-  // });
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3001);
 }
