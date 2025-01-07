69f887d603927da6ba4d18022b3e8e5a9b26da0d
AlexRudnyk <alexander.rudnyk@ukr.net>
2025-01-07 12:33:47 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: '*',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: true,
-  //   allowedHeaders: 'Content-Type, Accept, Authorization',
-  // });
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3001);
 }
